85f6fd1752acdc8432a10107216b3020224cd775
jennifer yb jones <jennifer.yb.jones@premisehealth.com>
2016-02-07 10:42:19 -0600

    manipulating the DOM and learning props

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render(){
-		return <div>Hello</div>
+		let txt = this.props.txt
+		// return <h1>{this.props.txt}</h1>
+		return <h1>{txt}</h1>
 	}
 }
 
-export default App
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps ={
+	txt: 'this is the default txt'
+}
+ReactDOM.render(
+	// <App cat={5} txt="this is the props value" />, this txt overwrites the defaultProps one, so take out
+	<App cat={5} />,
+	document.getElementById('app')
+	);
+
+
+/* THIS IS THE ORIGINAL BEFORE PROPS */
+
+// class App extends React.Component {
+// 	render(){
+// 		return (
+// 			<div>
+// 			<h1>Hello World</h1>
+// 			<b>Bold</b>
+// 			</div>
+// 			);
+		
+// 	}
+// }
+
+// export default App
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/* To render app directly from the app.js ONLY use import App from 'App' */
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('app'))
+
+
+/* ORIGINAL SETUP */
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// ReactDOM.render(<App />, document.getElementById('app'))
