c07f608dd324df645670478275bd1b524da77d78
jennifer yb jones <jennifer.yb.jones@premisehealth.com>
2016-02-07 10:55:53 -0600

    states

--- App.js
+++ App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { 
+			txt: 'this is the state txt',
+			cat: 0
+		}
+	}
+	update(e){
+		this.setState({txt: e.target.value})
+	}
 	render(){
-		let txt = this.props.txt
-		// return <h1>{this.props.txt}</h1>
-		return <h1>{txt}</h1>
+		return (
+			<div>
+			<input type ="text"
+				onChange={this.update.bind(this)} />
+			<h1>{this.state.txt}</h1>
+			</div>
+		);
 	}
 }
+export default App
+/*=========================================================
+=          THis is how to create/write props            =
+=========================================================*/
 
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
+// import ReactDOM from 'react-dom';
+// class App extends React.Component {
+// 	render(){
+// 		let txt = this.props.txt
+// 		// return <h1>{this.props.txt}</h1>
+// 		return <h1>{txt}</h1>
+// 	}
+// }
 
-App.defaultProps ={
-	txt: 'this is the default txt'
-}
-ReactDOM.render(
-	// <App cat={5} txt="this is the props value" />, this txt overwrites the defaultProps one, so take out
-	<App cat={5} />,
-	document.getElementById('app')
-	);
+// App.propTypes = {
+// 	txt: React.PropTypes.string,
+// 	cat: React.PropTypes.number.isRequired
+// }
+
+// App.defaultProps ={
+// 	txt: 'this is the default txt'
+// }
+// ReactDOM.render(
+// 	// <App cat={5} txt="this is the props value" />, this txt overwrites the defaultProps one, so take out
+// 	<App cat={5} />,
+// 	document.getElementById('app')
+// 	);
+
+/*=====  End of THis is how to create/write props  ======*/
 
 
 /* THIS IS THE ORIGINAL BEFORE PROPS */
